19fdb95fb51ca31d04a8e6a4e59855f480366e48
Evgeniy Kokaiko <70027656+re1nhart01@users.noreply.github.com>
2024-01-03 01:58:38 +0200

    Update OrbitControls.js

--- src/OrbitControls.js
+++ src/OrbitControls.js
@@ -13,7 +13,7 @@
 //    Zoom - middle mouse, or mousewheel / touch: two finger spread or squish
 //    Pan - right mouse, or arrow keys / touch: three finger swipe
 
-THREE.OrbitControls = function ( object, domElement ) {
+const OrbitControls = function ( object, domElement ) {
 
 	this.object = object;
 
